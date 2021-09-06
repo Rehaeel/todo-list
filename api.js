fc893aa276ac6e8a3aa351dd1474ff42467f9eac
ProkopiakR <rafal.prokopiak@ttpsc.pl>
2021-09-06 17:06:25 +0200

    deleting crap

---
+++ api.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+const mystery = mysql.createConnection({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+});
+
+/////////// GET
+app.get('/todolist', (req, res) => {
+    mystery.query('SELECT * FROM todo-list', (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+////////// POST
+
+app.post('/todolist', (req, res) => {
+    mystery.query('SELECT * FROM todo-list', (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+});
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`My DB run on ${PORT} port`);
+});
