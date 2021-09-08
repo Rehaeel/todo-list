8890ebb5f6b82560e75ee5b4bb6f6f67e5cf3033
Rafał Prokopiak <Rafal.Prokopiak@ttpsc.pl>
2021-09-08 11:31:27 +0200

    adding favicon component

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Favicon from 'react-favicon';
+import favicon from './component/favicon.svg';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Favicon url={favicon} />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
