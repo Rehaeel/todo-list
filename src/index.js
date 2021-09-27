c58b00f97bd68cf1e5fee73c594c189f41c3cee5
Rafał Prokopiak <rafal.prokopiak@ttpsc.pl>
2021-09-27 18:21:10 +0200

    adding Google Analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_KEY);
 
 ReactDOM.render(
   <React.StrictMode>
