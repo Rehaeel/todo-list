c7139fc1514c228cd618eb338fa391eabfc32504
Rafał Prokopiak <88146956+Rehaeel@users.noreply.github.com>
2022-06-14 13:24:58 +0200

    cleanup

--- src/helperFunctions.js
+++
@@ -1,9 +0,0 @@
-export const returnUpdatedTasks = (tasks, fetched) =>
-	fetched.filter((task) => {
-		const foundEl = tasks.find((el) => el.id === task.id);
-		if (foundEl) return task.zadanie !== foundEl.zadanie;
-		else return true;
-	});
-
-export const compareArrs = (tasks, newTasks) =>
-	newTasks.filter((e) => !tasks.some((e2) => e.id === e2.id));
