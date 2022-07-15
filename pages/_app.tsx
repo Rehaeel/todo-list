2bd6d611ec55cd3db274f68dfdb7f7855f886e8a
Rafał Prokopiak <88146956+Rehaeel@users.noreply.github.com>
2022-07-15 17:31:39 +0200

    Add favicon and title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>Todo Lista</title>
+				<link
+					rel='icon'
+					type='image/x-icon'
+					href='/images/logo.png'></link>
+			</Head>
+			<Component {...pageProps} />;
+		</>
+	);
 }
 
 export default MyApp;
